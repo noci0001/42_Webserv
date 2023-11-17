b19be2cfb48682cce49a470cef422d822c8ad2cd
tstrassb <tstrassb@student.42wolfsburg.de>
2023-11-17 16:51:30 +0100

    upload feature

---
+++ cgi-bin/upload.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import cgi, os
+
+form = cgi.FieldStorage()
+
+fileitem = form['filename']
+
+if fileitem.filename:
+    open(os.getcwd() + '/cgi-bin/upload/' + os.path.basename(fileitem.filename), 'wb').write(fileitem.file.read())
+    message = 'The file "' + os.path.basename(fileitem.filename) + '" was uploaded successfully'
+else:
+    message = 'No file was uploaded'
+
+print("Content-Type: text/html; charset=utf-8")
+print("Content-Type: text/html\r\n")
+print("<h1> " + message + " </h1>")
