1173c3a162fbdc1ae2013f2b52ffb0bfb0d461fd
snocita <snocita@student.42wolfsburg.de>
2023-11-17 16:52:41 +0100

    changes blah blah

--- cgi-bin/post_movie.py
+++ cgi-bin/post_movie.py
@@ -1,34 +1,59 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
-import requests
+import os
 import cgi
-import pdb; pdb.set_trace()
+import cgitb
+import errno 
 
-# Server details
-url = "http://localhost/index/uploads/"  # Replace with your actual server and endpoint
+cgitb.enable()
 
-# Receive the file from the client
+upload_dir = './cgi-bin/uploads/'
+
+# Check if the directory already exists
+if not os.path.exists(upload_dir):
+    try:
+        # Create the directory
+        os.makedirs(upload_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            # If the error is not due to the directory already existing, raise it
+            raise
+
 form = cgi.FieldStorage()
+print(form.keys())  # Print all keys in the form
+file_item = form.getvalue('file')
+print(file_item)  # Print the value of the 'file' key
 
-if 'file' in form:
-    file_item = form['file']
+if file_item:
+    file_name = os.path.basename(file_item.filename)
+    file_path = os.path.join(upload_dir, file_name)
 
-    # Get the filename
-    filename = file_item.filename
+    with open(file_path, 'wb') as file:
+        file.write(file_item.file.read())
 
-    print(f"Uploaded filename: {filename}")
-    echo
+    file_item = cgi.FieldStorage()
+    print(form.keys())  # Print all keys in the form
+    file_item = form.get('file')
+    print(file_item)  # Print the value of the 'file' key
 
-    # Send the file to the server
-    files = {'file': (filename, file_item.file)}
-    data = {'key': 'value'}  # Additional data if needed
-
-    # Send the POST request
-    response = requests.post("index/uploads/", files=files, data=data)
-
-    # Print the server's response
-    print(response.text)
-
-    print("File successfully uploaded to the server.")
+    print("Content-Type: text/html")
+    print()
+    print("<html>")
+    print("<head><title>File Upload</title></head>")
+    print("<body>")
+    print("<h1>File Upload Successful</h1>")
+    print("<p>Uploaded file: {0}</p>".format(file_name))
+    print("</body>")
+    print("</html>")
 else:
-    print("No file uploaded.")
+    print("Content-Type: text/html")
+    print()
+    print("<html>")
+    print("<head><title>File Upload</title></head>")
+    print("<body>")
+    print(form.keys()) 
+    print(file_item)
+    print("<h1>File Upload Failed</h1>")
+    print("<p>No file was uploaded.</p>")
+    print("</body>")
+    print("</html>")
